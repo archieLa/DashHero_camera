aa40cebe42b4e37a250a52c175597fec6f5abad3
archiela <arturm.lach@gmail.com>
2023-10-10 00:02:39 -0500

    Initial Commit

---
+++ DashHero_camera/DashHero_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashHero_cameraApp.swift
+//  DashHero_camera
+//
+//  Created by Kasia Lach on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DashHero_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
